5035cde3d7c3b024a82a90e0b1ba8a5ab279793e
laurenoo <lauren.ooghe31@gmail.com>
2026-02-01 13:15:37 -0700

    Add files via upload
    ChatGPT code

--- 4-bit_DAC/ramp.ino
+++ 4-bit_DAC/ramp.ino
@@ -1,1 +1,31 @@
+/*
+  100 Hz Ramp Test for 4-bit DAC
+  Pins: 3,4,5,6  (LSB -> MSB)
+*/
+
+const uint8_t dacPins[4] = {3, 4, 5, 6};
+
+inline void writeDAC4(uint8_t code) {
+  code &= 0x0F;
+  digitalWrite(dacPins[0], (code >> 0) & 1);
+  digitalWrite(dacPins[1], (code >> 1) & 1);
+  digitalWrite(dacPins[2], (code >> 2) & 1);
+  digitalWrite(dacPins[3], (code >> 3) & 1);
+}
+
+void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(dacPins[i], OUTPUT);
+    digitalWrite(dacPins[i], LOW);
+  }
+}
+
+void loop() {
+  const unsigned int stepDelay_us = 313;   //  µs per code
+
+  for (uint8_t code = 0; code < 16; code++) {
+    writeDAC4(code);
+    delayMicroseconds(stepDelay_us);
+  }
+}
 
